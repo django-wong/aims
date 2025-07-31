dbdf603b0529c80bd3740517881338142c11d611
Django Wong <me@djangowong.com>
2025-07-31 11:10:44 +0800

    feat: update user loading logic to handle potential null values

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -47,7 +47,7 @@ class HandleInertiaRequests extends Middleware
                 'author' => trim($author)
             ],
             'auth' => [
-                'user' => $request->user()->load('user_role'),
+                'user' => $request->user()?->load('user_role'),
                 'org' => $request->user()?->org
             ],
             'flash' => [
