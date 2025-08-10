9c7f7651b4ada6931837705a03e9c1d50464e1c9
Django Wong <me@djangowong.com>
2025-08-11 01:55:15 +0800

    feat: update user button label for clarity

--- resources/js/pages/users.tsx
+++ resources/js/pages/users.tsx
@@ -122,8 +122,7 @@ export default function Users() {
       pageAction={
         <UserForm onSubmit={() => {}}>
           <Button>
-            {' '}
-            <Plus/>Add User
+            <Plus/> New
           </Button>
         </UserForm>
       }>
